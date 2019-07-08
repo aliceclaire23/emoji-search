c24e75579e86441b0e7c8dcaabf9c051515a6164
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-08 18:21:07 +0100

    add description

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Heading = () => {
-  return <h1>Emoji Search</h1>;
+  return (<h1>Emoji Search</h1>
+    <p>Use the search bar to filter the emojis, click on an emoji to copy it to the clipboard.</p>);
 };
 
 export default Heading;
