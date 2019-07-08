ec2a942ef396eeffc161d8e9f1e95ab5418d9141
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-08 18:22:29 +0100

    amended heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const Heading = () => {
-  return (<h1>Emoji Search</h1>
-    <p>Use the search bar to filter the emojis, click on an emoji to copy it to the clipboard.</p>);
+  return (
+    <div>
+      <h1>Emoji Search</h1>
+      <p>
+        Use the search bar to filter the emojis, click on an emoji to copy it to
+        the clipboard.
+      </p>
+    </div>
+  );
 };
 
 export default Heading;
